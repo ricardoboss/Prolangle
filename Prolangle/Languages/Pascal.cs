311acdbe4f2f12e5ebb149c3cb9610c138434445
Ricardo Boss <mail@ricardoboss.de>
2024-01-01 22:23:16 +0100

    Changed syntax style to pascal

--- Prolangle/Languages/Pascal.cs
+++ Prolangle/Languages/Pascal.cs
@@ -9,7 +9,7 @@ public class Pascal : BaseLanguage
 	public override TypeSystem Typing { get; } = TypeSystem.Static | TypeSystem.Strong | TypeSystem.Safe;
 	public override bool Compiled { get; } = true;
 	public override MemoryManagement MemoryManagement { get; } = MemoryManagement.GarbageCollection;
-	public override SyntaxStyle SyntaxStyle { get; } = SyntaxStyle.Perl;
+	public override SyntaxStyle SyntaxStyle { get; } = SyntaxStyle.Pascal;
 	public override Applications KnownForBuilding { get; } = Applications.Education | Applications.Desktop;
 	public override Paradigms Paradigms { get; } = Paradigms.Imperative | Paradigms.Structured | Paradigms.Procedural | Paradigms.ObjectOriented | Paradigms.Generic;
 	public override double? TiobeRating { get; } = 0.92;
