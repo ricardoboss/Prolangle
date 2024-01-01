deee08245212495c1d68d811797bea13798303c9
Ricardo Boss <mail@ricardoboss.de>
2024-01-01 16:54:07 +0100

    Add Dart to LanguagesProvider

--- Prolangle/Services/LanguagesProvider.cs
+++ Prolangle/Services/LanguagesProvider.cs
@@ -18,6 +18,7 @@ public class LanguagesProvider
 			yield return new Cpp();
 			yield return new CSharp();
 			yield return new Css();
+			yield return new Dart();
 			yield return new Html();
 			yield return new Java();
 			yield return new Javascript();
