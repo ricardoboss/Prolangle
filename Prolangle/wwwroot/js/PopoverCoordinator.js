86f4b3354abad48681643056123bc492dff048ea
Sören Kuklau <kuklau@trenz.de>
2024-01-02 13:10:25 +0100

    31-single-popover: unify creating/destroying popovers

--- Prolangle/wwwroot/js/PopoverCoordinator.js
+++ Prolangle/wwwroot/js/PopoverCoordinator.js
@@ -1,16 +1,27 @@
 export class PopoverCoordinator {
-static showOrReplace(elem, optionsJson) {
-    const options = JSON.parse(optionsJson);
+    static existingInstanceElement;
 
-    bootstrap.Popover.getOrCreateInstance(elem,
-        options);
-}
+    static showOrReplace(elem, optionsJson) {
+        const options = JSON.parse(optionsJson);
 
-static destroy(elem) {
-    const tooltip = bootstrap.Popover.getInstance(elem);
+        console.log(`showing with title ${options.title}`)
 
-    if (tooltip != null && typeof tooltip !== 'undefined') {
-        tooltip.dispose();
+        if (this.existingInstanceElement != null && typeof this.existingInstanceElement !== 'undefined') {
+            this.destroy(this.existingInstanceElement);
+        }
+
+        bootstrap.Popover.getOrCreateInstance(elem, options);
+
+        this.existingInstanceElement = elem;
     }
-}
+
+    static destroy(elem) {
+        const popover = bootstrap.Popover.getInstance(elem);
+
+        if (popover != null && typeof popover !== 'undefined') {
+            console.log(`destroying with title ${popover._config.title}`)
+
+            popover.dispose();
+        }
+    }
 }
