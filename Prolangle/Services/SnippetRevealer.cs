80a142fac2c0f58e92df7053bc310af5a0238819
Sören Kuklau <kuklau@trenz.de>
2024-01-12 10:41:38 +0100

    Snippets game: bring back jitter, and fix an edge case where `minValue` underflowed

--- Prolangle/Services/SnippetRevealer.cs
+++ Prolangle/Services/SnippetRevealer.cs
@@ -2,7 +2,10 @@ using System.Text.RegularExpressions;
 
 namespace Prolangle.Services;
 
-public partial class SnippetRevealer(GuessGame guessGame, string sourceCode)
+/// <param name="guessGame">The underlying game. Also used as seed source for jitter.</param>
+/// <param name="sourceCode">The source code to conceal and subsequently reveal.</param>
+/// <param name="useJitter">Whether to introduce random shift, rather than always reveal code from the same center.</param>
+public partial class SnippetRevealer(GuessGame guessGame, string sourceCode, bool useJitter)
 {
 	private double RevealedProgress { get; set; } = 0.0;
 
@@ -17,16 +20,21 @@ public partial class SnippetRevealer(GuessGame guessGame, string sourceCode)
 		var revealStart = (int)(concealedCode.Length / 2f - concealedCode.Length * RevealedProgress / 2);
 		var revealEnd = (int)(concealedCode.Length / 2f + concealedCode.Length * RevealedProgress / 2);
 
-		// // randomly move the center of revealed code around a little, to make snippets harder to remember
-		// var random = new Random(guessGame.Seeder());
-		// const int padding = 3;
-		// var jitter = random.Next((revealStart - padding) * -1, revealStart - padding);
-		//
-		// revealStart += jitter;
-		// revealEnd += jitter;
+		if (useJitter)
+		{
+			// randomly move the center of revealed code around a little, to make snippets harder to remember
+			var random = new Random(guessGame.Seeder());
+			const int padding = 3;
+			int shiftedStart = revealStart - padding > 0 ? revealStart - padding : revealStart;
+			var jitter = random.Next(shiftedStart * -1, shiftedStart);
+			Console.WriteLine($"Jitter {jitter}");
+
+			revealStart += jitter;
+			revealEnd += jitter;
+		}
 
 		concealedCode = concealedCode[..revealStart] + sourceCode.Substring(revealStart, revealEnd - revealStart) +
-		             concealedCode[revealEnd..];
+		                concealedCode[revealEnd..];
 
 		return concealedCode;
 	}
