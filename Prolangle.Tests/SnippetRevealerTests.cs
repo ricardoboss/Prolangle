afb372b0e9b5c5d1453da21dcf61cd1d54b93507
Sören Kuklau <kuklau@trenz.de>
2024-01-12 10:41:38 +0100

    Snippets game: bring back jitter, and fix an edge case where `minValue` underflowed

--- Prolangle.Tests/SnippetRevealerTests.cs
+++ Prolangle.Tests/SnippetRevealerTests.cs
@@ -4,7 +4,7 @@ namespace Prolangle.Tests;
 
 public class SnippetRevealerTests
 {
-	public static IEnumerable<object[]> SnippetData
+	public static IEnumerable<object[]> TestData
 	{
 		get { yield return ["Console.WriteLine(message);",
 							"••••••••••••eL•••••••••••••",
@@ -19,20 +19,61 @@ public class SnippetRevealerTests
 	}
 
 	[Theory]
-	[MemberData(nameof(SnippetData))]
-	public void Test1(string input,
-					  string expected10PercentOutput,
-					  string expected20PercentOutput,
-					  string expected30PercentOutput,
-					  string expected40PercentOutput,
-					  string expected50PercentOutput,
-					  string expected60PercentOutput,
-					  string expected70PercentOutput,
-					  string expected80PercentOutput,
-					  string expected90PercentOutput)
+	[MemberData(nameof(TestData))]
+	public void Test(string input,
+		string expected10PercentOutput,
+		string expected20PercentOutput,
+		string expected30PercentOutput,
+		string expected40PercentOutput,
+		string expected50PercentOutput,
+		string expected60PercentOutput,
+		string expected70PercentOutput,
+		string expected80PercentOutput,
+		string expected90PercentOutput)
 	{
-		var game = new GuessGame(null!, () => DateTime.Now.Minute);
-		var revealer = new SnippetRevealer(game, input);
+		var game = new GuessGame(null!, () => 1_234);
+		var revealer = new SnippetRevealer(game, input, useJitter: false);
+
+		Assert.Equal(expected10PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected20PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected30PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected40PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected50PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected60PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected70PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected80PercentOutput, revealer.RevealMore());
+		Assert.Equal(expected90PercentOutput, revealer.RevealMore());
+	}
+
+	public static IEnumerable<object[]> TestDataWithJitter
+	{
+		get { yield return ["Console.WriteLine(message);",
+							"••••••••••it•••••••••••••••",
+							"••••••••WriteL•••••••••••••",
+							"•••••••.WriteLi••••••••••••",
+							"••••••e.WriteLin•••••••••••",
+							"•••••le.WriteLine(m••••••••",
+							"••••ole.WriteLine(me•••••••",
+							"•••sole.WriteLine(mes••••••",
+							"•onsole.WriteLine(messa••••",
+							"Console.WriteLine(messag•••"]; }
+	}
+
+	[Theory]
+	[MemberData(nameof(TestDataWithJitter))]
+	public void TestWithJitter(string input,
+							   string expected10PercentOutput,
+							   string expected20PercentOutput,
+							   string expected30PercentOutput,
+							   string expected40PercentOutput,
+							   string expected50PercentOutput,
+							   string expected60PercentOutput,
+							   string expected70PercentOutput,
+							   string expected80PercentOutput,
+							   string expected90PercentOutput)
+	{
+		var game = new GuessGame(null!, () => 1_234);
+		var revealer = new SnippetRevealer(game, input, useJitter: true);
 
 		Assert.Equal(expected10PercentOutput, revealer.RevealMore());
 		Assert.Equal(expected20PercentOutput, revealer.RevealMore());
