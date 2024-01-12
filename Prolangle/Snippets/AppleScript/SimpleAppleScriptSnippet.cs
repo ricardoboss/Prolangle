11263fb46e6d461447adcc680b736246dc73dcfb
Sören Kuklau <kuklau@trenz.de>
2024-01-12 22:17:53 +0100

    Snippets game: snippets now declare their own language, and `ICodeSnippet` uses reflection to discover them

--- Prolangle/Snippets/AppleScript/SimpleAppleScriptSnippet.cs
+++ Prolangle/Snippets/AppleScript/SimpleAppleScriptSnippet.cs
@@ -1,7 +1,11 @@
+using Prolangle.Languages.Framework;
+
 namespace Prolangle.Snippets.AppleScript;
 
 public class SimpleAppleScriptSnippet : ICodeSnippet
 {
+	public ILanguage Language => Languages.AppleScript.Instance;
+
 	public string SourceCode =>
 		"""
 		set message to "Hello World!"
