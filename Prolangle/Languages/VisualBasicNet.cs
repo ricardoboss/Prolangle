47bfcf829dbe5964da386faf8fcb95a0833de209
Sören Kuklau <kuklau@trenz.de>
2024-01-15 10:14:17 +0100

    appearance years for VB were backwards

--- Prolangle/Languages/VisualBasicNet.cs
+++ Prolangle/Languages/VisualBasicNet.cs
@@ -29,5 +29,5 @@ public class VisualBasicNet : BaseLanguage
 	                                               Paradigms.EventDriven | Paradigms.Reflective;
 
 	public override double? TiobeRating { get; } = 1.60;
-	public override int AppearanceYear { get; } = 1991;
+	public override int AppearanceYear { get; } = 2001;
 }
