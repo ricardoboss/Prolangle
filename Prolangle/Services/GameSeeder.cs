f8643beaa38d8a235dc0eee249010030955aff3e
Sören Kuklau <kuklau@trenz.de>
2024-01-22 13:38:48 +0100

    Countdown until next game. Fixes #88

--- Prolangle/Services/GameSeeder.cs
+++ Prolangle/Services/GameSeeder.cs
@@ -1,6 +1,7 @@
 namespace Prolangle.Services;
 
-public class GameSeeder(Func<int> seeder)
+public class GameSeeder(Func<int> seeder, DateTime currentGametimestamp, DateTime nextGameTimestamp)
 {
 	public Func<int> Seeder { get; } = seeder;
+	public DateTime NextGameTimestamp { get; } = nextGameTimestamp;
 }
