492b1dd35bad32e3847225fd60948ff8021b044d
Ricardo Boss <mail@ricardoboss.de>
2024-03-10 00:06:32 +0100

    Fix tests
    (cherry picked from commit 8226cdd6366b4dd6599c93115ab69fb73dce8e8c)

--- Prolangle.Tests/GuessGameTests.cs
+++ Prolangle.Tests/GuessGameTests.cs
@@ -25,7 +25,7 @@ public class GuessGameTests
 
 	[Theory]
 	[InlineData(1_000, "C++", "Assembly")]
-	[InlineData(1_234, "Java", "Dart")]
+	[InlineData(1_234, "JavaScript", "Dart")]
 	public void TestLanguages(int seed, string expectedMetadatumGameLanguage, string expectedSnippetGameLanguage)
 	{
 		var seeder = new GameSeeder(() => seed, DateTime.MinValue, DateTime.MinValue);
