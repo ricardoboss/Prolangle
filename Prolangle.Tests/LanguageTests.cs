425d3fcaf26b57db205170ffe18dbf5690738e9f
Sören Kuklau <kuklau@trenz.de>
2024-03-30 19:41:59 +0100

    add unit tests to make missing languages less likely

---
+++ Prolangle.Tests/LanguageTests.cs
@@ -0,0 +1,50 @@
+using System.Reflection;
+using NetArchTest.Rules;
+using Prolangle.Languages.Framework;
+using Prolangle.Services;
+
+namespace Prolangle.Tests;
+
+public class LanguageTests
+{
+	[Fact]
+	public void AllLanguagesInheritBaseLanguage()
+	{
+		ConditionList? allInheritBaseLanguage =
+			Types.InAssembly(typeof(BaseLanguage).Assembly)
+				.That().ResideInNamespace("Prolangle.Languages")
+				.And().DoNotResideInNamespaceStartingWith("Prolangle.Languages.")
+				.Should().Inherit(typeof(BaseLanguage));
+
+		Assert.True(allInheritBaseLanguage.GetResult().IsSuccessful);
+	}
+
+	[Fact]
+	public void AllLanguageConstructorsArePrivate()
+	{
+		IEnumerable<ConstructorInfo> constructors =
+			Types.InAssembly(typeof(BaseLanguage).Assembly)
+			.That().Inherit(typeof(BaseLanguage))
+			.GetTypes()
+			.SelectMany(t => t.GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+
+		Assert.True(constructors.All(ctor=>!ctor.IsPublic));
+	}
+
+	/// <summary>
+	/// For now, the properties game (and therefore LanguagesProvider) will
+	/// always offer _all_ languages (unlike the snippets game), so we expect
+	/// these counts to match.
+	/// </summary>
+	[Fact]
+	public void LanguagesProviderMatchesCount()
+	{
+		var expectedLanguages = Types.InAssembly(typeof(BaseLanguage).Assembly)
+			.That().Inherit(typeof(BaseLanguage))
+			.GetTypes();
+
+		var providedLanguages = new LanguagesProvider().Languages;
+
+		Assert.Equal(expectedLanguages.Count(), providedLanguages.Count());
+	}
+}
