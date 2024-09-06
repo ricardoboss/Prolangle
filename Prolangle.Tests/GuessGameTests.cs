d7416f0f1ee7df0e9e419032e61c0693c3c066c3
Sören Kuklau <kuklau@trenz.de>
2024-09-06 22:36:08 +0200

    fix tests

--- Prolangle.Tests/GuessGameTests.cs
+++ Prolangle.Tests/GuessGameTests.cs
@@ -24,9 +24,9 @@ public class GuessGameTests
 	private LanguagesProvider LanguagesProvider { get; }
 
 	[Theory]
-	[InlineData(1_000, "Swift", "XML")]
-	[InlineData(1_001, "HTML", "CSS")]
-	[InlineData(1_234, "PowerShell", "STEP")]
+	[InlineData(1_000, "Scratch", "PHP")]
+	[InlineData(1_001, "Go", "CSS")]
+	[InlineData(1_234, "PowerShell", "Markdown")]
 	public void TestLanguages(int seed, string expectedMetadatumGameLanguage, string expectedSnippetGameLanguage)
 	{
 		var seeder = new GameSeeder(() => seed, DateTime.MinValue, DateTime.MinValue);
