9e4d93254a4bc136b6c4f024abfb6dfce296f213
Sören Kuklau <kuklau@trenz.de>
2024-09-06 22:37:07 +0200

    scale language weight against provided languages

--- Prolangle.Tests/LanguageWeightTests.cs
+++ Prolangle.Tests/LanguageWeightTests.cs
@@ -7,26 +7,59 @@ namespace Prolangle.Tests;
 
 public class LanguageWeightTests
 {
-	[Theory]
-
-	// language has no data
-	[InlineData(null, 100)]
-	[InlineData(0.0, 100)]
-
-	// fictional languages
-	[InlineData(0.5, 99)]
-	[InlineData(1.0, 99)]
-	[InlineData(20.0, 5)]
-
-	// real-world examples
-	[InlineData(0.47, 99)] // Dart
-	[InlineData(0.79, 99)] // Rust
-	[InlineData(1.11, 90)] // Assembly
-	[InlineData(2.9, 34)] // JS
-	[InlineData(11.44, 8)] // C
-	public void TestLanguageWeight(double? tiobeRating, int expectedWeight)
+	/// <summary>
+	/// Languages that don't have data
+	/// </summary>
+	public static IEnumerable<object?[]> NoData
 	{
-		var actualWeight = LanguageEnumerablePickExtensions.GetWeightFromTiobeRating(tiobeRating);
+		get
+		{
+			List<double?> emptyData = [];
+
+			yield return [null, 100, emptyData];
+
+			yield return [0.0, 100, emptyData];
+		}
+	}
+
+	/// <summary>
+	/// Made-up language data
+	/// </summary>
+	public static IEnumerable<object?[]> FictionalData
+	{
+		get
+		{
+			double?[] ratings = [0.5, 1.0, 20.0];
+			double?[] expectedWeights = [40, 20, 1];
+
+			for (int i = 0; i < ratings.Length; i++)
+				yield return [ratings[i], expectedWeights[i], ratings];
+		}
+	}
+
+	/// <summary>
+	/// Examples from our actual languages
+	/// </summary>
+	public static IEnumerable<object?[]> RealWorldData
+	{
+		get
+		{
+			// Dart, Rust, Assembly, JS, C
+			double?[] ratings = [0.47, 0.79, 1.11, 2.9, 11.44];
+			double?[] expectedWeights = [24, 14, 10, 3, 1];
+
+			for (int i = 0; i < ratings.Length; i++)
+				yield return [ratings[i], expectedWeights[i], ratings];
+		}
+	}
+
+	[Theory]
+	[MemberData(nameof(NoData))]
+	[MemberData(nameof(FictionalData))]
+	[MemberData(nameof(RealWorldData))]
+	public void TestLanguageWeight(double? tiobeRating, int expectedWeight, IEnumerable<double?> allRatings)
+	{
+		var actualWeight = LanguageEnumerablePickExtensions.GetWeightFromTiobeRating(tiobeRating, allRatings);
 
 		Assert.Equal(expectedWeight, actualWeight);
 	}
--- Prolangle/Extensions/LanguageEnumerablePickExtensions.cs
+++ Prolangle/Extensions/LanguageEnumerablePickExtensions.cs
@@ -6,13 +6,14 @@ namespace Prolangle.Extensions;
 
 public static class LanguageEnumerablePickExtensions
 {
-	public static ILanguage PickWeightedRandom(this IEnumerable<ILanguage> languages,
+	public static ILanguage PickWeightedRandom(this IReadOnlyList<ILanguage> languages,
 		GameSeeder seeder)
 	{
 		var randomizer = new StaticWeightedRandomizer<ILanguage>(seeder.Seeder());
 
 		foreach (ILanguage language in languages)
-			randomizer.Add(language, GetWeightFromTiobeRating(language.TiobeRating));
+			randomizer.Add(language,
+				GetWeightFromTiobeRating(language.TiobeRating, languages.Select(l => l.TiobeRating)));
 
 		return randomizer.NextWithReplacement();
 	}
@@ -22,11 +23,18 @@ public static class LanguageEnumerablePickExtensions
 	/// which the language should appear. I.e., _more commonly_-used
 	/// languages become the language to guess _less_ often.
 	/// </summary>
-	internal static int GetWeightFromTiobeRating(double? tiobeRating)
+	internal static int GetWeightFromTiobeRating(double? tiobeRating, IEnumerable<double?> allRatings)
 	{
+		var normalizedRatings = allRatings.Select(NormalizeToo100);
+
 		if (tiobeRating is null or 0)
 			return 100;
 
-		return (int)Math.Min(100.0 / tiobeRating.Value, 99.0);
+		var scaleFactor = 99.0 / normalizedRatings.Max();
+
+		return (int)Math.Min(100.0 / (tiobeRating.Value * scaleFactor), 99.0);
+
+		static double NormalizeToo100(double? input)
+			=> input is null or 0 ? 100.0 : input.Value;
 	}
 }
