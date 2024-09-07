6c26febc7c2b2d38f3856231721f886f31b7565e
Sören Kuklau <kuklau@trenz.de>
2024-09-07 14:37:06 +0200

    IAttributedCodeSnippet: make license optional

--- Prolangle/Snippets/IAttributedCodeSnippet.cs
+++ Prolangle/Snippets/IAttributedCodeSnippet.cs
@@ -9,8 +9,8 @@ public interface IAttributedCodeSnippet : ICodeSnippet
 	string Attribution { get; }
 
 	/// <summary>
-	/// The license for this code snippet. Will be revealed when the user has
-	/// won.
+	/// The license for this code snippet. If set, will be revealed when the
+	/// user has won.
 	/// </summary>
-	string License { get; }
+	string? License { get; }
 }
