dbceeac74fd537afd6175aa483470e28dba4a343
Sören Kuklau <kuklau@trenz.de>
2024-09-07 14:29:29 +0200

    make code snippets attributable

---
+++ Prolangle/Snippets/IAttributedCodeSnippet.cs
@@ -0,0 +1,16 @@
+namespace Prolangle.Snippets;
+
+public interface IAttributedCodeSnippet : ICodeSnippet
+{
+	/// <summary>
+	/// The attribution for this code snippet. Will be revealed when the user
+	/// has won.
+	/// </summary>
+	string Attribution { get; }
+
+	/// <summary>
+	/// The license for this code snippet. Will be revealed when the user has
+	/// won.
+	/// </summary>
+	string License { get; }
+}
