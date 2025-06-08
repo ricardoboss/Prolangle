88d5ed2dace10330991acd3fca60fe1d1a12693c
Soeren Kuklau <chucker23n+github@gmail.com>
2025-06-08 02:44:47 +0200

    Added Logo

---
+++ Prolangle/Languages/Logo.cs
@@ -0,0 +1,26 @@
+using Prolangle.Languages.Framework;
+
+namespace Prolangle.Languages;
+
+public class Logo : BaseLanguage
+{
+	private Logo()
+	{
+	}
+
+	public static Logo Instance { get; } = new();
+
+	public override Guid Id { get; } = Guid.NewGuid();
+	public override string Name { get; } = "Logo";
+	public override TypeSystem Typing { get; } = TypeSystem.Dynamic;
+	public override bool Compiled { get; } = false;
+	public override MemoryManagement MemoryManagement { get; } = MemoryManagement.None;
+	public override SyntaxStyle SyntaxStyle { get; } = SyntaxStyle.Other;
+
+	public override Applications KnownForBuilding { get; } = Applications.Education | Applications.Fun;
+
+	public override Paradigms Paradigms { get; } = Paradigms.Imperative;
+
+	public override double? TiobeRating { get; } = 0.14;
+	public override int AppearanceYear { get; } = 1967;
+}
